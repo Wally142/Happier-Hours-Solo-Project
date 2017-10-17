7792bb7714ed4033c974c537e7bddd30ffdee040
Greg.Wallerus <gwallerus@gmail.com>
2017-10-16 22:51:01 -0500

    small additions, view set up

---
+++ database.sql
@@ -0,0 +1,22 @@
+-- CREATE TABLE "users" (
+--  "id" serial primary key,
+--  "username" varchar(80) not null UNIQUE,
+--  "password" varchar(240) not null
+-- );
+
+-- CREATE TABLE happy (
+-- 	id serial primary key,
+-- 	"Location" varchar (100) UNIQUE,
+-- 	"Day" varchar (20),
+-- 	"Time" varchar (20),
+-- 	Specials varchar (300),
+-- 	"Comments" varchar (500),
+-- 	user_id int REFERENCES users (id)
+-- 	);
+
+-- CREATE TABLE comments (
+-- 	id serial primary key,
+-- 	"Comments" varchar (500),
+-- 	user_id int REFERENCES users (id)
+-- 	);
+
